ba030f3e2c5a01b2092c63a276b5a659fdd1915a
kanda <kandavelece906@gmail.com>
2026-01-29 20:30:53 +0530

    python variables

---
+++ sample.py
@@ -0,0 +1,18 @@
+print ("kandu")
+
+x = 10
+y = 20
+print(x+y)
+
+print(y, type(y))
+# x = 'Hi'
+# print(x+y)
+
+if x >= 10:
+    print('X is greater')
+
+x = 'Hi'
+print(x, type(x))
+
+x = 9.12
+print(x,type(x))
